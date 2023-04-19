dce0ff0f20083ae292940806fbebba6924aaeeeb
sungjinnn <127401478+sungjinnn@users.noreply.github.com>
2023-04-19 21:02:12 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,9 @@
+for i in range(1, 10):
+    for j in range(2, 6):
+        print(f"{j} x {i} = {j*i}", end="\t")
+    print()
+print('\n')
+for i in range(1, 10):
+    for j in range(6, 10):
+        print(f"{j} x {i} = {j*i}", end="\t")
+    print()
