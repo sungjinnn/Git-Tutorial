7a9c5ecc0040a292173b7a2846dd6d938a063039
sungjinnn <127401478+sungjinnn@users.noreply.github.com>
2023-04-19 21:06:26 +0900

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -2,7 +2,7 @@ for i in range(1, 10):
     for j in range(2, 6):
         print(f"{j} x {i} = {j*i}", end="\t")
     print()
-print('\n')
+print()
 for i in range(1, 10):
     for j in range(6, 10):
         print(f"{j} x {i} = {j*i}", end="\t")
